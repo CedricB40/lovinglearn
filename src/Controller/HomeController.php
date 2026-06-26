ef21f4f3f65d722dcc9361e10d7019032d9a54b2
CedricB40 <bazinc40@gmail.com>
2026-06-26 17:39:14 +0200

    page sujet par thème

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ThemeRepository;
+use App\Repository\SubjectRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -16,6 +17,24 @@ class HomeController extends AbstractController
 
         return $this->render('home/index.html.twig', [
             'themes' => $themes,
+        ]);
+    }
+
+    #[Route(path: '/theme/{slug}', name: 'app_theme_show_public')]
+    public function showTheme(string $slug, ThemeRepository $themeRepository, SubjectRepository $subjectRepository): Response
+    {
+        $theme = $themeRepository->findOneBy(['slug' => $slug]);
+
+        if (!$theme) {
+            $this->addFlash('error', 'Thème introuvable !');
+            return $this->redirectToRoute('app_home');
+        }
+
+        $subjects = $subjectRepository->findBy(['theme' => $theme]);
+
+        return $this->render('home/theme.html.twig', [
+            'theme' => $theme,
+            'subjects' => $subjects,
         ]);
     }
 }
